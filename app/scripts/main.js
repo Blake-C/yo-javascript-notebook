8da2decc0e378a14d7a8f7ab42808864d7cf99df
Blake Cerecero <Blake@DigitalBlake.com>
2016-08-02 23:29:35 -0500

    [html, js] Reorganized files
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

---
+++ app/scripts/main.js
@@ -0,0 +1,5 @@
+// import './modules/01_array-foreach-method.js';
+// import './modules/02_heigher-order-functions-filter.js';
+// import './modules/03_heigher-order-functions-map.js';
+// import './modules/04_heigher-order-functions-reduce.js';
+import './modules/05_heigher-order-functions-reduce-2.js';
