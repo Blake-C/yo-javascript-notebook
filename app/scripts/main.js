81aef88b73edd099e1f0038c6af7a24e994d3a0f
Blake Cerecero <Blake@DigitalBlake.com>
2016-08-07 16:03:17 -0500

    [js] Reorganized assets
    Signed-off-by: Blake Cerecero <Blake@DigitalBlake.com>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
-// import './modules/01_array-foreach-method.js';
-// import './modules/02_heigher-order-functions-filter.js';
-// import './modules/03_heigher-order-functions-map.js';
-// import './modules/04_heigher-order-functions-reduce.js';
-import './modules/05_heigher-order-functions-reduce-2.js';
+// import './modules/heigher-order-functions/01_array-foreach-method.js';
+// import './modules/heigher-order-functions/02_heigher-order-functions-filter.js';
+// import './modules/heigher-order-functions/03_heigher-order-functions-map.js';
+// import './modules/heigher-order-functions/04_heigher-order-functions-reduce.js';
+import './modules/heigher-order-functions/05_heigher-order-functions-reduce-2.js';
